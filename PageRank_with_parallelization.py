2c55971d1f1c20765793e59e8912941671065375
Tanya Zhang <88880703+JessicaGreenn@users.noreply.github.com>
2024-10-29 16:41:27 -0700

    PageRank with parallelization
    python source code

---
+++ PageRank_with_parallelization.py
@@ -0,0 +1,82 @@
+import sys
+from operator import add
+from typing import Iterable, Tuple
+from pyspark.resultiterable import ResultIterable
+from pyspark.sql import SparkSession
+import os
+import time
+
+os.environ['PYSPARK_PYTHON'] = sys.executable
+
+n_threads = 4  # Number of local threads
+n_iterations = 10  # Number of iterations
+q = 0.15 #the default value of q is 0.15
+
+def computeContribs(neighbors: ResultIterable[int], rank: float) -> Iterable[Tuple[int, float]]:
+    # Calculates the contribution(rank/num_neighbors) of each vertex, and send it to its neighbours.
+    num_neighbors = len(neighbors)
+    for vertex in neighbors:
+        yield (vertex, rank / num_neighbors)
+
+def read_graph(file_path):
+    edges = []  # List to store the tuples (FromNodeId, ToNodeId)
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Skip lines that start with '#'
+            if line.startswith('#'):
+                continue
+
+            # Split the line by tab or space and convert the two values to integers
+            parts = line.split()
+            if len(parts) == 2:  # Ensure there are two elements (FromNodeId, ToNodeId)
+                from_node = int(parts[0])
+                to_node = int(parts[1])
+                edges.append((from_node, to_node))  # Add the tuple to the list
+
+    return edges
+
+# Initialize the spark context.
+startTime = time.time()
+file_path = 'web-Google.txt'
+edges_list = read_graph(file_path)
+spark = SparkSession\
+    .builder\
+    .appName("PageRank")\
+    .master("local[%d]" % n_threads)\
+    .getOrCreate()
+
+# link: (source_id, dest_id)
+links = spark.sparkContext.parallelize(
+    edges_list,
+)
+
+# drop duplicate links and convert links to an adjacency list.
+adj_list = links.distinct().groupByKey().cache()
+
+# count the number of vertexes
+n_vertexes = adj_list.count()
+
+# init the rank of each vertex, the default is 1.0/n_vertexes
+ranks = adj_list.map(lambda vertex_neighbors: (vertex_neighbors[0], 1.0/n_vertexes))
+
+# Calculates and updates vertex ranks continuously using PageRank algorithm.
+for t in range(n_iterations):
+    # Calculates the contribution(rank/num_neighbors) of each vertex, and send it to its neighbours.
+    contribs = adj_list.join(ranks).flatMap(lambda vertex_neighbors_rank: computeContribs(
+        vertex_neighbors_rank[1][0], vertex_neighbors_rank[1][1]  # type: ignore[arg-type]
+    ))
+
+    # Re-calculates rank of each vertex based on the contributions it received
+    ranks = contribs.reduceByKey(add).mapValues(lambda rank: q/n_vertexes + (1 - q)*rank)
+
+# Collects all ranks of vertexs and dump them to console.
+for (vertex, rank) in ranks.collect():
+    print("%s has rank: %s." % (vertex, rank))
+
+spark.stop()
+endTime = time.time()
+print(endTime-startTime)
+
+    
+
